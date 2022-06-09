6618e8767ba416d205a162877a94d31278d49d83
luquisinger <matheus.779@hotmail.com>
2022-06-09 18:31:58 -0300

    Update CalcController.js

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -24,11 +24,28 @@ class CalcController {
 
     }
 
+    addEventListenerAll(element, events, fn){
+
+        events.split(' ').forEach(event => {
+            element.addEventListener(event, fn, false);
+        });
+
+    }
+
     initButtonsEvents(){
+
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
-        buttons.addEventListener('click', e=>{
-                console.log(buttons);
-        })
+        
+        buttons.forEach((btn, index)=>{  
+            this.addEventListenerAll(btn, "click drag mouseover", e=>{
+                    console.log(btn.className.baseVal.replace("btn-",""));
+            });
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e =>{
+
+                btn.style.cursor = "pointer";
+
+            })
+        });    
     }
 
     setDisplayDateTime(){
